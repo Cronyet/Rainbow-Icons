d822febd23d95b36d49722f87c3c49b6fb4f6fa7
Dynesshely <2369913161@qq.com>
2022-03-21 15:42:21 +0800

    💾 Feat(Demo): Create Demo Project.

---
+++ Interaga/Rainbow Icons/Interaga.Demo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interaga.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool IsDark = false;
+
+        private void Background_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsDark)
+            {
+                Background = new SolidColorBrush(Colors.Black);
+                IsDark = true;
+            }
+            else
+            {
+                Background = new SolidColorBrush(Colors.White);
+                IsDark = false;
+            }
+        }
+    }
+}
